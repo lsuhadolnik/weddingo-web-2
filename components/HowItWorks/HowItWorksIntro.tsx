54fed6a428ef703eee2ec5a1ab9149804db01130
Lovro Suhadolnik <lovros16@gmail.com>
2021-12-31 18:35:28 +0100

    WIP - Animation

---
+++ components/HowItWorks/HowItWorksIntro.tsx
@@ -0,0 +1,36 @@
+import CallToAction from 'components/Buttons/CallToAction';
+import Header from 'components/Header';
+import Image from 'next/image';
+import styles from 'styles/HowItWorksIntro.module.css';
+
+interface IProps {
+    siteParts: {[key: string]: string}
+}
+
+const HowItWorksIntro = (props: IProps) => {
+
+
+    return <div className={styles.SlideWrap}>
+        <div className={styles.LeftWrap}>
+            
+            <div className={styles.TextsWrap}>
+                <div className={styles.MainTitle}>Kako deluje?</div>
+                <div className={styles.Subtitle}>Weddingo je tvoja rešitev za prekratek poročni dan</div>
+                
+                
+                <CallToAction className={styles.CTAWrap}>Preizkusi zdaj!</CallToAction>
+
+                
+                <div className={styles.Subtitle}>
+                    Na tej strani lahko izveš, kako deluje.
+                    Če te še karkoli zanima, pa nam napiši na tvojdan@weddingo.si
+                </div>
+            </div>
+        </div>
+        <div className={styles.RightWrap}>
+            <Image src='/images/howItWorksIntroGraphics.svg' layout='fill' objectPosition={'cover'} objectFit={'cover'}/>
+        </div>
+    </div>
+}
+
+export default HowItWorksIntro;
--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,9 +2,11 @@ import Nav from "./Nav";
 
 import styles from 'styles/Layout.module.css';
 import Footer from "./Footer";
+import { CSSProperties } from "react";
 
 interface IProps {
     children: any;
+    style?: CSSProperties;
 }
 
 const Layout = (props: IProps) => {
@@ -12,7 +14,7 @@ const Layout = (props: IProps) => {
 
     return <div>
         <Nav />
-        <div className={styles.MainMargin}>
+        <div className={styles.MainMargin} style={props.style}>
             {props.children}
         </div>
         <Footer />
