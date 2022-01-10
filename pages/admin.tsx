f0bb121a10a681766b42bd6973e2e0397669667f
Lovro Suhadolnik <lovros16@gmail.com>
2022-01-10 08:56:29 +0100

    Changed cms settings

---
+++ pages/admin.tsx
@@ -0,0 +1,41 @@
+interface IProps {
+
+}
+
+import MasterLoader from 'components/Markdown/MasterLoader';
+import { useEffect } from 'react';
+import dynamic from 'next/dynamic';
+import Head from 'next/head';
+import parseMarkdown from 'lib/parseMarkdown';
+
+const CMS = dynamic(() => import('netlify-cms-app').then((cms) => {
+    debugger;
+    const _cms = cms as any;
+    _cms.init();
+
+    _cms.registerPreviewTemplate('pages', (props: any) => {
+        const entry = props.entry.get('raw');
+        const {content, data} = parseMarkdown(entry);
+        return <MasterLoader markdown={content} />
+    });
+
+    return cms.NetlifyCmsApp as any;
+}), {ssr: false});
+
+interface IProps {
+
+}
+
+const admin = (props: IProps) => {
+
+
+    return <>
+        <Head>
+            <link href="/admin/config.yml" type="text/yaml" rel="cms-config-url" />
+            <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+        </Head>
+        <CMS />
+    </>
+}
+
+export default admin;
